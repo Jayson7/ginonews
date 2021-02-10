9fdf154dff93661719991374ca7c8aeecc617670
jayson7 <lexxiijoo70@gmail.com>
2021-02-09 19:56:53 -0800

    gini

--- apps/views.py
+++ apps/views.py
@@ -15,10 +15,22 @@ def logins(request):
     return render(request, 'login.html')
 
 
-
+@login_required(login_url="/login/")
 def log(request):
+
     logss = logger.objects.all()
-    return render(request, 'profile.html', {"logss": logss})
+    firstname = logss[0].first_name
+    lastname = logss[0].last_name
+    phone = logss[0].phone_number
+    occupation = logss[0].occupation
+    email = logss[0].email
+    username = logss[0].username
+   
+    return render(request, 'profile.html', {"logss": logss, "firstname": firstname,": lastname": lastname,
+    "occupation": occupation,
+    "email": email,
+    "username": username,
+    "phone": phone })
 
 
 
